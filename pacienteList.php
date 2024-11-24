ab46cf3b4defb07181d0d1f5f1b4a0765fd5925e
AndersonCamposs <anderson.camposs87@outlook.com>
2024-11-24 16:32:06 -0300

    Conclusão da funcionalidade de adicionar e listar os usuários

--- pacienteList.php
+++ pacienteList.php
@@ -70,19 +70,6 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/ac_clinic/model/dao/PacienteDAO.php';
                                             "</tr>";
                                         }
                                         ?>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Helder Nunes</td>
-                                            <td>30/05/2000</td>
-                                            <td>123.456.789-10</td>
-                                            <td>10982136</td>
-                                            <td>87 99677-9122</td>
-                                            <td>helder@email.com</td>
-                                            <td>
-                                                <a href="#" class="btn btn-outline-warning"><i class="fas fa-pen"></i>Editar</a>
-                                                <a href="#" class="btn btn-outline-danger"><i class="fas fa-trash"></i>Apagar</a>
-                                            </td>
-                                        </tr>                                        
                                     </tbody>
                                 </table>
                             </div>
