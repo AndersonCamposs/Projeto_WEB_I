13ade7316cb5361b5dbdc1ce9c24789039148575
Anderson Campos <anderson.camposs87@outlook.com>
2024-11-28 10:36:27 -0300

    Salvando as imagens enviadas no banco de dados

--- controller/usuarioController.php
+++ controller/usuarioController.php
@@ -4,14 +4,40 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/ac_clinic/model/dao/UsuarioDAO.php';
 
 if(isset($_POST['nome'])) {
     
-    $repetirSenha = $_POST["repetirSenha"];
-
     $usuario = new UsuarioVO();
+    $repetirSenha = $_POST["repetirSenha"];
+    //PROCESSANDO A FOTO ENVIADA PELO USUÁRIO
+    if (isset($_FILES["foto"])) {
+        $foto = $_FILES["foto"];
+        
+        $extensoesPermitidas = ["jpg", "jpeg", "png"];
+        $extensaoFoto = strtolower(pathinfo($foto["name"], PATHINFO_EXTENSION));
+        
+        if(!in_array($extensaoFoto, $extensoesPermitidas)) {
+            die("Erro: extensão de foto não permitida");
+        } else {
+            $uploadDir = __DIR__."/../uploads/users/";
+            if (!is_dir($uploadDir)) {
+                // VERIFICA SE O DIRETÓRIO DE UPLOADS EXISTS, SENÃO CRIA
+                mkdir($uploadDir, 0755, true);
+            }
+            $fileName = uniqid("img_").".".$extensaoFoto;
+            
+            $fullUploadName = $uploadDir.$fileName;
+            
+            if(move_uploaded_file($foto["tmp_name"], $fullUploadName)){
+                $usuario->setFoto("./uploads/users/".$fileName);
+            } else {
+                $usuario->setFoto(null);
+            }
+        }
+    }
+    
+    
     $usuario->setNome($_POST["nome"]);
     $usuario->setEmail($_POST["email"]);
     $usuario->setCpf($_POST["cpf"]);
     $usuario->setSenha($_POST["senha"]);
-    $usuario->setFoto($_POST["foto"]);
     
     if(isset($_POST['id'])) {
         $usuario->setId($_POST['id']);
