78a9a5d59949d78c62c64d7ee041f6ee6fc1f76d
AndersonCamposs <anderson.camposs87@outlook.com>
2024-12-02 18:21:34 -0300

    Componentizando o rodapé

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; AndersonCamposs</div>
+            <div>
+                <a href="https://github.com/AndersonCamposs/Projeto_WEB_I" target="_blank">Repositório</a>
+                &middot;
+                <a href="https://github.com/AndersonCamposs" target="_blank">GitHub</a>
+            </div>
+        </div>
+    </div>
+</footer>
--- pacienteList.php
+++ pacienteList.php
@@ -79,18 +79,9 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/ac_clinic/model/dao/PacienteDAO.php';
                         </div>
                     </div>
                 </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+                <?php
+                include("./footer.php");
+                ?>
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
