f2a3ab48b3c81e1905a0c578f41fac0ab65d81ff
Anderson Campos <anderson.camposs87@outlook.com>
2024-12-04 00:07:25 -0300

    Criando o globalFormValidator para organizar melhor a validação dos formulários

---
+++ js/globalFormValidator.js
@@ -0,0 +1,6 @@
+const consultaAddEditForm = document.getElementById("consultaAddEditForm");
+const medicoAddEditForm = document.getElementById("medicoAddEditForm");
+const pacienteAddEditForm = document.getElementById("pacienteAddEditForm");
+const usuarioAddEditForm = document.getElementById("usuarioAddEditForm");
+
+console.log(consultaAddEditForm);
