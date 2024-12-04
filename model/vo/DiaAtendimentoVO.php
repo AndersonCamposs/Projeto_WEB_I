3e1ad17e0cbba63b81042a6a0e250969e7b8f871
Anderson Campos <anderson.camposs87@outlook.com>
2024-12-04 19:09:01 -0300

    Criando o VO da entidade de dia de atendimento e da entidade associativa que surge do relacionamento n para n entre a entidade médico e a entidade dia de atendimento

---
+++ model/vo/DiaAtendimentoVO.php
@@ -0,0 +1,27 @@
+<?php
+
+class DiaAtendimentoVO {
+    private $id;
+    private $nome;
+    
+    public function __construct() {
+        
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setId($id): void {
+        $this->id = $id;
+    }
+
+    public function setNome($nome): void {
+        $this->nome = $nome;
+    }
+
+}
